e5c4cddb10ed785c421e40e6e5b903979ec991eb
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-03-24 19:49:37 -0300

    add cli config

--- client/index.js
+++ client/index.js
@@ -1,7 +1,21 @@
+/**
+ * node index.js \
+ *  --username gabriel \
+ *  --room sala01
+ *  --hostUri localhost
+ */
+
 import Events from 'events'
 import TerminalController from './src/terminalController.js'
+import CliConfig from './src/cliConfig.js'
+
+const [nodePath, filePath, ...commands] = process.argv
+
+const config = CliConfig.parseArguments(commands)
+
+console.log(config)
 
 const componentEmitter = new Events()
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
